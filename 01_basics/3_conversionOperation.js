c54c36dbf349655bb272cc38184931dd7ea9824b
Agyagupta <131612369+Agyagupta@users.noreply.github.com>
2025-06-05 11:09:02 +0000

    conversion and operation done

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // if given value is in string and we try to convert in number, then it cant be convert but js will show that is convert and give o/p as a NaN!!
 // if value is null then its show 0 !!
@@ -20,7 +20,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
  
 //  1 => true; 0 => false;
 // for empty string "" => false 
@@ -29,6 +29,57 @@ console.log(booleanIsLoggedIn);
 let someNumber = 45
 
 let stringSomeNumber = String (someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+
+
+// **********************************  Operations  *********************************
+
+let value = 3
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**4);   
+// console.log(4/2);
+// console.log(10%2);    
+
+let str1 = "hello"
+let str2 = " Agya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);    // 122
+// console.log(1 + 2 + "2");    // 32
+
+
+// console.log(+true);  //o/p-1
+// console.log(true+);  // unexpected tokan
+// console.log(+"");    //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+gameCounter++;
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
 
